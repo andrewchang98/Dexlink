07ca210b8f56ce4d5a87c01f84f25fa843d3db1e
andrewchang98 <andrewchang99@gmail.com>
2022-04-01 09:01:27 -0700

    Increased verobisty even more and added exception handling for login.

--- Examples/print-current-bg.py
+++ Examples/print-current-bg.py
@@ -1,19 +1,29 @@
 print('Importing API...')
 from pydexcom import Dexcom
-#from datetime import datetime
+
+
 print('Logging in...')
-dexcom = Dexcom('staciepeterson', 'Finn2019312!')
-print('Logged in as "staciepeterson"')
+try:
+    dexcom = Dexcom('staciepeterson', 'Finn2019312!')
+    print('Logged in as "staciepeterson".')
+except AccountError:
+    print('Login failed. Please verify password and check internet connection.')
+    print('Exiting.')
+    exit()
+
 
 print('Getting current BG.')
 bg = dexcom.get_current_glucose_reading()
 if(bg == None):
-    print('No recent readings availale, getting last BG within 24 hours.')
+    print('!!!WARNING: ATTEMPT FAILED. GETTING LAST BG WITHIN 24 HOURS!!!')
     bg = dexcom.get_latest_glucose_reading()
     if(bg == None):
-        print('No glucose readings available within the last 24 hours.')
+        print('No glucose readings available within the last 24 hours!')
+        print('Please check if Dexcom and Dexlink are working correctly.')
         print('Exiting.')
         exit()
+print('Success.')
+
 
 timestamp = bg.time
 print(bg.value, 'mg/dL', bg.trend_arrow, timestamp.strftime('%c'))
