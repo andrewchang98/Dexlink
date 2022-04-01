3dfcfe61c97b5b656d469cee49b92a45225e2ac8
andrewchang98 <andrewchang99@gmail.com>
2022-04-01 08:48:58 -0700

    Increased verbosity, added trend arrow.

--- Examples/print-current-bg.py
+++ Examples/print-current-bg.py
@@ -1,10 +1,14 @@
+print('Importing API...')
 from pydexcom import Dexcom
 #from datetime import datetime
-
+print('Logging in...')
 dexcom = Dexcom('staciepeterson', 'Finn2019312!')
+print('Logged in as "staciepeterson"')
 
+print('Getting current BG.')
 bg = dexcom.get_current_glucose_reading()
 if(bg == None):
+    print('No recent readings availale, getting last BG within 24 hours.')
     bg = dexcom.get_latest_glucose_reading()
     if(bg == None):
         print('No glucose readings available within the last 24 hours.')
@@ -12,4 +16,4 @@ if(bg == None):
         exit()
 
 timestamp = bg.time
-print(bg.value, 'mg/dL', timestamp.strftime('%c'))
+print(bg.value, 'mg/dL', bg.trend_arrow, timestamp.strftime('%c'))
