cfbc4e6d24a6ca99a5fa4a964105a835ddb55ff6
Andrew Chang <andrewchang99@gmail.com>
2022-04-06 03:09:04 -0700

    modified login

--- utilities/login.py
+++ utilities/login.py
@@ -1,61 +1,52 @@
-"""
-Logs into Dexcom
-
-"""
 import sys
 from getpass import getpass
+from pydexcom import Dexcom
 
-
-#Import API
-try:
-    print('Loading API...')
-    from pydexcom import Dexcom
-except:
-    print('Missing or damaged API module.')
-    print('Exiting.')
-    exit()
-
-#Login option if account info is avaiable
-def auth(pair):
+# PROMPT USER INFO
+def prompt():
+    print('Log in to Dexcom:')
     try:
-        global DEXCOM_USERNAME
-        global DEXCOM_PASSWORD
-        print('Login as {}?'.format(pair[0]), end = ' ')
-        keystroke = input('(Y/n): ')
-        if keystroke == 'y' or keystroke == 'Y':
-            DEXCOM_USERNAME = pair[0]
-            DEXCOM_PASSWORD = pair[1]
-        elif keystroke == 'n' or keystroke == 'N':
-            DEXCOM_USERNAME = input('Username: ')
-            DEXCOM_PASSWORD = getpass('Password: ')
-        else:
-            auth(pair)
+        username = input('Username: ')
+        password = getpass('Password: ')
     except KeyboardInterrupt:
-        print()
-        print('Canceled by user. Exiting.')
+        print('\n', 'Canceled by user. Exiting now.', sep='')
         sys.exit(0)
+    else:
+        return username, password
 
-#Load account info
-try:
+# LOAD ACCOUNT INFO
+def account():
+    # TRY TO LOAD PASSWORDS FILE
     print('Loading account info...')
-    from accounts import passwords
-    auth(passwords.account)
-except ImportError:
     try:
+        from accounts import passwords
+    except ImportError:
         print('No account info in ~/.local/lib/python3.9/site-packages/')
-        DEXCOM_USERNAME = input('Username: ')
-        DEXCOM_PASSWORD = getpass('Password: ')
-    except KeyboardInterrupt:
-        print()
-        print('Canceled by user. Exiting.')
+        DEXCOM_USERNAME, DEXCOM_PASSWORD = prompt()
+    else:
+        # ASK TO LOGIN AS USER
+        print('Login as {} (y/n)?'.format(passwords.account[0]), end = ' ')
+        try:
+            keystroke = input()
+            if keystroke == 'y' or keystroke == 'Y':
+                DEXCOM_USERNAME = passwords.account[0]
+                DEXCOM_PASSWORD = passwords.account[1]
+            elif keystroke == 'n' or keystroke == 'N':
+                DEXCOM_USERNAME, DEXCOM_PASSWORD = prompt()
+            else:
+                print('\n', 'Canceled by user. Exiting now.', sep='')
+                sys.exit(0)
+        except KeyboardInterrupt:
+            print('\n', 'Canceled by user. Exiting now.', sep='')
+            sys.exit(0)
+    # CONNECT TO DEXCOM SHARE API
+    try:
+        print('\n', 'Connecting to Dexcom servers...', '\n', sep='')
+        dexcom = Dexcom(DEXCOM_USERNAME, DEXCOM_PASSWORD)
+    except:
+        print('\n', 'Login failed. Please check username and password.', sep='')
+        print('Exiting now.')
         sys.exit(0)
-
-#Login to Dexcom
-try:
-    print('Connecting to Dexcom servers... \n')
-    dexcom = Dexcom(DEXCOM_USERNAME, DEXCOM_PASSWORD)
-except:
-    print()
-    print('Login failed. Please check username and password.')
-else:
-    print('Logged in as {}'.format(DEXCOM_USERNAME))
+    else:
+        print('\n', 'Logged in as {}'.format(DEXCOM_USERNAME), '\n', sep='')
+        return dexcom
