0b8ddf5602ac3c137e1a3409fd9e0bd6d2992092
Andrew Chang <andrewchang99@gmail.com>
2022-04-08 16:28:37 -0700

    edited msg

--- examples/getcurrentbg.py
+++ examples/getcurrentbg.py
@@ -8,5 +8,5 @@ last_reading = 0
 while True:
     if time() - last_reading >= 300:
         bg = dexcom.get_current_glucose_reading()
-        print(bg.value, 'mg/dL', bg.trend_description, bg.time.strftime('%I:%M %p %A %B %d, %Y'))
+        print(bg.value, 'mg/dL', bg.trend_description, '|', bg.time.strftime('%I:%M%p %A %B %d, %Y'))
         last_reading = time()
