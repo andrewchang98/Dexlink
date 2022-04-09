9682d4ef03fa160c4c380bba0e96f4a2a4cd9a1b
Andrew Chang <andrewchang99@gmail.com>
2022-04-08 22:43:03 -0700

    added keyboardinterrupt exception

--- getcurrentbg.py
+++ getcurrentbg.py
@@ -1,3 +1,4 @@
+from sys import exit
 from login import login
 from time import time
 from slowprinter import Printer
@@ -7,11 +8,15 @@ ui = Printer(delay=0.05)
 
 last_reading = 0
 while True:
-    if time() - last_reading >= 300:
-        bg = dexcom.get_current_glucose_reading()
-        ui.printer(str(bg.value), end=' ')
-        ui.printer('mg/dL', end=' ')
-        ui.printer(bg.trend_description, end=' ')
-        ui.printer('|', end=' ')
-        ui.printer(bg.time.strftime('%I:%M%p %A %B %d, %Y'), end='\n\n')
-        last_reading = time()
+    try:
+        if time() - last_reading >= 300:
+            bg = dexcom.get_current_glucose_reading()
+            ui.printer(str(bg.value), end=' ')
+            ui.printer('mg/dL', end=' ')
+            ui.printer(bg.trend_description, end=' ')
+            ui.printer('|', end=' ')
+            ui.printer(bg.time.strftime('%I:%M%p %A %B %d, %Y'), end='\n\n')
+            last_reading = time()
+    except KeyboardInterrupt:
+        print('User KeyboardInterrupt. Exiting now.')
+        exit(0)
