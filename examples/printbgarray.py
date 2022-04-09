5a98655df7b21d67794b80990a5d4228172128b1
Andrew Chang <86166050+andrewchang98@users.noreply.github.com>
2022-04-08 20:55:11 -0700

    Delete printbgarray.py

--- examples/printbgarray.py
+++
@@ -1,9 +0,0 @@
-import printCurrentBG
-
-#BG_HOURS = input('Enter desired timeframe in hours: ')
-#BG_MAX_COUNT = input('Enter maximum number of data points desired: ')
-
-#print('Retrieving {} BG datapoints over the last {} hours.'.format(hours, max_count))
-#BG_MINUTES = BG_HOURS * 60
-#bg = dexcom.get_glucose_readings(BG_MINUTES, BG_MAX_COUNT)
-#print(len(bg))
