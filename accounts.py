f183f38dff587bce6f653bef6b5191b9517c17b2
Andrew Chang <86166050+andrewchang98@users.noreply.github.com>
2022-04-08 21:08:38 -0700

    Update accounts.py

--- accounts.py
+++ accounts.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 
+# ACCOUNT CLASS FOR STORING LOGIN INFO
 class Account:
-    def __init__(self, api_key, secret_key):
-        self.api_key = api_key
-        self.secret_key = secret_key
-        self.time = datetime.now()
-
-    def get_api_key()
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.created = datetime.now()
