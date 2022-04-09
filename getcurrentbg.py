81fce1b92a98ffa067f9a6b6d55d9293a46b7a98
Andrew Chang <andrewchang99@gmail.com>
2022-04-08 22:36:05 -0700

    bug

--- getcurrentbg.py
+++ getcurrentbg.py
@@ -3,16 +3,16 @@ from time import time
 from slowprinter import Printer
 
 dexcom = login()
-printer = ui.Printer(delay=0.05)
+ui = Printer(delay=0.05)
 
 last_reading = 0
 while True:
     if time() - last_reading >= 300:
         bg = dexcom.get_current_glucose_reading()
         print(bg.value, 'mg/dL', bg.trend_description, '|', bg.time.strftime('%I:%M%p %A %B %d, %Y'))
-        printer(str(bg.value), end=' ')
-        printer('mg/dL', end=' ')
-        printer(bg.trend_description, end=' ')
-        printer('|', end=' ')
-        printer(bg.time.strftime('%I:%M%p %A %B %d, %Y'), end='\n\n')
+        ui.printer(str(bg.value), end=' ')
+        ui.printer('mg/dL', end=' ')
+        ui.printer(bg.trend_description, end=' ')
+        ui.printer('|', end=' ')
+        ui.printer(bg.time.strftime('%I:%M%p %A %B %d, %Y'), end='\n\n')
         last_reading = time()
