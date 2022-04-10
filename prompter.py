9a11a6607f4c8367e066e6e666484eda616fa058
Andrew Chang <andrewchang99@gmail.com>
2022-04-10 04:33:12 -0700

    changed prompter

--- prompter.py
+++ prompter.py
@@ -3,16 +3,17 @@ from getpass import getpass
 from slowprinter import Printer
 
 # ACCOUNT SUBROUTINE TO PROMPT USER INFO
-def prompter(printer=None):
+def prompter(printer=None, message='Log into Dexcom:'):
     if printer is None:
         printer = print
     else:
         printer = printer
-    printer('Log into Dexcom:')
+    printer(message)
     try:
-        printer('Username:', end=' ')
+        printer('\nUsername:', end=' ')
         username = input()
-        password = getpass()
+        printer('\nPassword:', end=' ')
+        password = getpass(prompt='')
     except KeyboardInterrupt:
         print('\nCancelled by user. Exiting now.')
         sys.exit(0)
