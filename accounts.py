ee751ba2ff7ddef88eba2033df26ad23e0d90a14
Andrew Chang <andrewchang99@gmail.com>
2022-04-10 17:34:17 -0700

    deleted accounts.py

--- accounts.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-# ACCOUNT CLASS FOR STORING LOGIN INFO
-class Account:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.created = datetime.now()
